6fb0e8e847966140b9a3f7fffcf15263da81cc35
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-01-21 22:26:43 +0200

    changed connection to mongo function

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
-import carsRoutes from './routes/cars.js';
-import mongoose from 'mongoose';
-import userRoutes from './routes/user.js';
-import reservationsRoutes from './routes/reservations.js';
+import dotenv from "dotenv";
+import carsRoutes from "./routes/cars.js";
+import mongoose from "mongoose";
+import userRoutes from "./routes/user.js";
+import reservationsRoutes from "./routes/reservations.js";
+import { connectToDb, PORT } from "./db";
 import path from "path";
 
 dotenv.config();
@@ -12,44 +13,50 @@ dotenv.config();
 // Creating an instance of the express app
 const app = express();
 app.use(express.static(path.join(__dirname, "../", "public")));
-app.use(express.json());
 app.use(
   cors({
     origin: "https://car-rent-app-iota.vercel.app",
-  }),
+  })
 );
 
 // Middleware for parsing JSON requests
+app.use(express.json());
 
-// app.use((req, res, next) => {
-//     console.log(req.path, req.method);
-//     next();
-// });
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-// // Middleware for handling CORS headers
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', ['*']);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     if ('OPTIONS' === req.method) {
-//         res.sendStatus(200);
-//     }
-//     else {
-//         next();
-//     }
-// });
+// Middleware for handling CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", ["*"]);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if ("OPTIONS" === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 // Setting up routes for different parts of the application
-app.use('/api/cars', carsRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/reservations', reservationsRoutes);
+app.use("/api/cars", carsRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/reservations", reservationsRoutes);
 
 // Connecting to the MongoDB database
-mongoose.connect(process.env.URI)
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log('listening on port', process.env.PORT)
-        })
-    })
-    .catch((err) => console.log(err));
+// mongoose.connect(process.env.URI)
+//     .then(() => {
+//         app.listen(process.env.PORT, () => {
+//             console.log('listening on port', process.env.PORT)
+//         })
+//     })
+//     .catch((err) => console.log(err));
+connectToDb()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database", err);
+  });
