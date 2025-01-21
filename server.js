a5785d0947e5bffc9fff2e07510a11f465425f2c
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-01-21 22:17:25 +0200

    changes in serverjs

--- server.js
+++ server.js
@@ -12,29 +12,33 @@ dotenv.config();
 // Creating an instance of the express app
 const app = express();
 app.use(express.static(path.join(__dirname, "../", "public")));
-app.use(cors());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://car-rent-app-iota.vercel.app",
+  }),
+);
 
 // Middleware for parsing JSON requests
-app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method);
+//     next();
+// });
 
-// Middleware for handling CORS headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ['*']);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    if ('OPTIONS' === req.method) {
-        res.sendStatus(200);
-    }
-    else {
-        next();
-    }
-});
+// // Middleware for handling CORS headers
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', ['*']);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     if ('OPTIONS' === req.method) {
+//         res.sendStatus(200);
+//     }
+//     else {
+//         next();
+//     }
+// });
 
 // Setting up routes for different parts of the application
 app.use('/api/cars', carsRoutes);
