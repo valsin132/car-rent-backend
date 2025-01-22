37f4d4823c1e344ef3f58f18ccc9938135d97eb8
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-01-22 19:56:49 +0200

    deleted src

--- server.js
+++ server.js
@@ -27,23 +27,23 @@ app.use(cors());
 // Middleware for parsing JSON requests
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.path, req.method);
+//   next();
+// });
 
-// Middleware for handling CORS headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ["*"]);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if ("OPTIONS" === req.method) {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+// // Middleware for handling CORS headers
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", ["*"]);
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   if ("OPTIONS" === req.method) {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
 // Setting up routes for different parts of the application
 app.use("/api/cars", carsRoutes);
