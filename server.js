81ab6459218c98d29c3475adc69b1d72d5a5fe54
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-01-22 19:58:30 +0200

    deleted src

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 // Creating an instance of the express app
 const app = express();
-app.use(express.static(path.join(__dirname, "../", "public")));
+
 app.use(cors());
 // app.use(
 //   cors({
@@ -27,28 +27,30 @@ app.use(cors());
 // Middleware for parsing JSON requests
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   console.log(req.path, req.method);
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-// // Middleware for handling CORS headers
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", ["*"]);
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   if ("OPTIONS" === req.method) {
-//     res.sendStatus(200);
-//   } else {
-//     next();
-//   }
-// });
+// Middleware for handling CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", ["*"]);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if ("OPTIONS" === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 // Setting up routes for different parts of the application
 app.use("/api/cars", carsRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/reservations", reservationsRoutes);
+
+app.use(express.static(path.join(__dirname, "../", "public")));
 
 
 // mongoose.connect(process.env.URI)
